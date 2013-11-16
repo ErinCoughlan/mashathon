2b3583ea1071eeaf98b99f5e2fd59dd90e5de592
Philip Davis <pdavis@hmc.edu>
2013-11-16 02:44:24 -0800

    idek

--- mashmaker/utilities.py
+++ mashmaker/utilities.py
@@ -2,12 +2,14 @@
 from os import walk
 from os.path import join
 from pyechonest import config,track
+from random import randint, choice
 
 import models
 
 config.ECHO_NEST_API_KEY = 'DY8SILMY87LES3ADW'
 
 myMusicPath = "music"
+myMusicList = []
 
 def GetMP3FileList():
     f = []
@@ -20,15 +22,37 @@ def GetMP3FileList():
 def AnalyzeMP3Files():
     foo = GetMP3FileList()
     for file in foo:
-        f = open(file)
-        theTrack = track.track_from_file(f, 'mp3')
-        theTrack.get_analysis()
-        trackObj = models.trackInfo(theTrack,file)
-        try: print theTrack.title
-        except: print "unknown track"
-        trackObj.save()
-        f.close()
+        AnalyzeMP3File(file)
 
+def AnalyzeMP3File(file):
+    f = open(file)
+    theTrack = track.track_from_file(f, 'mp3')
+    theTrack.get_analysis()
+    trackObj = models.trackInfo(theTrack,file)
+    try: print theTrack.title
+    except: print "unknown track"
+    trackObj.save()
+    f.close()
 
+def CreateSongQueue(firstSong):
+    myMusicList.append((firstSong, 0, None))
+    currentSong = firstSong
+    while True:
+        (nextSong, nextSongStartTime, currentSongEndTime) = ChooseNextSong(currentSong)
+        myMusicList[-1] = myMusicList[-1][:2] + (currentSongEndTime)
+        myMusicList.append((nextSong, nextSongStartTime, None))
+        currentSong = nextSong
 
+def GetNextSong(firstSong):
+    (nextSong, nextSongStartTime, previousSongEndTime) = ChooseNextSong(firstSong)
+    SendMP3OfSong(nextSong, nextSongStartTime)
 
+def ChooseNextSong(previousSong):
+    numSongs = models.trackInfo.objects.latest('id').id
+    nextSong = models.trackInfo.objects.filter(id=randint(1,numSongs))
+    nextSongStartTime = choice(eval(nextSong.beats))['start']
+    previousSongEndTime = choice(eval(previousSong.beats))['start']
+    return (nextSong, nextSongStartTime, previousSongEndTime)
+
+def SendMP3OfSong(nextSong, nextSongStartTime):
+    f = open(nextSong.filePath)
