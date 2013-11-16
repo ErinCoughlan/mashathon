067257bb5a322364d674cd4904edbd0f9f5bb971
erinkidd01 <erinkidd01@gmail.com>
2013-11-16 01:43:43 -0800

    audio upload working - double checking how to keep as an mp3

--- static/js/home.js
+++ static/js/home.js
@@ -5,18 +5,34 @@
 function upload(e) {
     // Change the text
     var textBox = document.getElementById('longInput');
-    var upload = document.getElementById('selector');
+    var input = document.getElementById('fileInput');
     var filename = $('input[type=file]').val().split('\\').pop();
     textBox.value = filename;
 
     // Upload the audio file
-    if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
-        alert('The File APIs are not fully supported in this browser.');
-        return;
-    } else {
-        var file = input.files[0];
-        var fr = new FileReader();
-        //fr.readAsText(file);
-        fr.readAsDataURL(file);
-   }
-}
+};
+
+function previewSong(songSrc) {
+    return '<audio src="' + songSrc + '" controls="controls">';
+};
+
+
+/** Submit handler through jquery */
+$(document).ready(function() {
+    //previewSong("song");
+
+    var fileInput = document.getElementById("fileInput");
+    var freader = new FileReader();
+
+    freader.onload = function(e) {
+        document.getElementById('player').src = e.target.result;
+        document.getElementById('player').play();
+    }
+
+    fileInput.onchange = function(e) {
+        var files = e.target.files;
+        freader.readAsDataURL(files[0]);
+
+        upload();
+    }
+});
