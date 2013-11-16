3ffc9db05f09b0555c102fd579e07fa151ffa366
erinkidd01 <erinkidd01@gmail.com>
2013-11-15 21:47:17 -0800

    Working super simple home page

--- mashmaker/views.py
+++ mashmaker/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import Context, loader
+from django.http import Http404
+
+
 
 # Create your views here.
+
+
+def home(request):
+    # homepage
+    #t = loader.get_template('home.html')
+    return render_to_response('mashmaker/home.html', {})
