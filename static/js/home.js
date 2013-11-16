c412a0becdb0979e55a7765395024e5adb0d4a57
erinkidd01 <erinkidd01@gmail.com>
2013-11-16 00:10:18 -0800

    getting file from upload

--- static/js/home.js
+++ static/js/home.js
@@ -2,8 +2,21 @@
  * Set the text to the filename and begin upload and analyze.
  */
 "use strict";
-function upload() {
+function upload(e) {
+    // Change the text
     var textBox = document.getElementById('longInput');
+    var upload = document.getElementById('selector');
     var filename = $('input[type=file]').val().split('\\').pop();
     textBox.value = filename;
+
+    // Upload the audio file
+    if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
+        alert('The File APIs are not fully supported in this browser.');
+        return;
+    } else {
+        var file = input.files[0];
+        var fr = new FileReader();
+        //fr.readAsText(file);
+        fr.readAsDataURL(file);
+   }
 }
